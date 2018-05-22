d0d697a38edaea6fb4a7730fd84db4673e9181d1
SchoolPC <>
2018-05-22 09:18:50 +0300

    Initial commit

---
+++ Documentation/Documentation/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documentation
+{
+    /// <summary>
+    /// This is the calculator class
+    /// </summary>
+    /// <remarks>
+    /// It only contains class for adding two numbers
+    /// </remarks>
+    class Calculator
+    {
+        /// <summary>Gets the value of PI </summary>
+        public static double PI { get;}
+
+        /// <summary>
+        /// Adds two doubles, and returns the result
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>The sum of the two provided numbers</returns>
+        /// <example>
+        /// <code>
+        /// double c = Calculator.Add(1, 4);
+        /// </code>
+        /// </example>
+        public double AddWhoNumbers(double a, double b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
---
+++ Documentation/Documentation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me two numbers:\nNumber one: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nSecond number: ");
+            double b = double.Parse(Console.ReadLine());
+
+            var calculator = new Calculator();
+            double answer = calculator.AddWhoNumbers(a, b);
+            Console.WriteLine("\n1st and 2nd number added together: " + answer);
+            Console.WriteLine("***");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
